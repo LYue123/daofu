7bbb3396251073e95e09ccc57cb6ee36d82dac12
tao.ning <light-white@outlook.com>
2025-05-19 15:58:04 +0800

    add api/category_goods

--- wxcloudrun/wsgi.py
+++ wxcloudrun/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import jd
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wxcloudrun.settings')
+
+appkey = os.getenv('JD_APP_KEY', '')
+secret = os.getenv('JD_SECRET_KEY', '')
+
+jd.setDefaultAppInfo(appkey, secret) # only need to init one time
 
 application = get_wsgi_application()
